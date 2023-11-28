5f6980435b44003e165053141112e5a7f2f29788
Wola Adedeji <wola.adedeji@gmail.com>
2023-11-28 09:54:42 -0800

    update code changes

--- .aws-sam/build/LambdaHTTPAPIGatewayAuthorizer/lambda_function.py
+++ .aws-sam/build/LambdaHTTPAPIGatewayAuthorizer/lambda_function.py
@@ -4,6 +4,22 @@ import uuid
 import json
 import boto3
 
+def get_secret():
+    secret_name = os.environ.get("API_GATEWAY_TOKEN")  # Name of the secret in Secrets Manager
+    region_name = os.environ.get("AWS_REGION")   # Your AWS region
+
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except Exception as e:
+        print(f"Error retrieving secret: {e}")
+        return None
+
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
+
 # This function checks if the IP_ADDRESS is in the allowed IP_RANGE
 def is_ip_allowed(IP_ADDRESS, IP_RANGE):
     # Check if any of the ranges is a CIDR block
@@ -19,11 +35,17 @@ def is_ip_allowed(IP_ADDRESS, IP_RANGE):
 def lambda_handler(event, context):
     print(event)
 
+    # Retrieve the secret
+    secret = get_secret()
+    if not secret or 'EXPECTED_AUTH_TOKEN' not in secret:
+        raise ValueError("Secret not found or invalid format in Secrets Manager")
+
+    expected_auth_token = secret['EXPECTED_AUTH_TOKEN']
     whitelisted_ips = json.loads(os.environ.get("IP_WHITELIST", '[]'))
-    print(whitelisted_ips)
+
     # Extract the source IP and Authorization header from the event
     source_ip = event['requestContext']['http']['sourceIp']
-    #auth_header = event['headers'].get('Authorization', '')
+    auth_header = event['headers'].get('Authorization', '')
     API_ID = event["requestContext"]["apiId"]
     ACC_ID = event["requestContext"]["accountId"]
     METHOD = event["requestContext"]["http"]["method"]
@@ -32,11 +54,12 @@ def lambda_handler(event, context):
     
      # Check if the source IP is allowed and the Authorization header is valid
     is_ip_valid = is_ip_allowed(source_ip, whitelisted_ips)
+    is_auth_valid = event["headers"]["authorization"] == expected_auth_token
+    #print(is_auth_valid)
+    #print(auth_header)
+    print(expected_auth_token)
 
-
-    #policy_effect = "Allow" if event["headers"]["authorizationtoken"] == "secretcode"  and is_ip_valid else "Deny"
-    
-    if event["headers"]["authorizationtoken"] == "secretcode"  and is_ip_valid:
+    if is_auth_valid and is_ip_valid:
         response = {
             "principalId": f"{uuid.uuid4().hex}",
             "policyDocument": {
@@ -49,7 +72,6 @@ def lambda_handler(event, context):
                     }
                 ],
             },
-            "context": {"exampleKey": "exampleValue"},
         }
 
         return response
@@ -66,26 +88,24 @@ def lambda_handler(event, context):
                 }
             ],
         },
-        "context": {"exampleKey": "exampleValue"},
     }
 
     return response
-
-
+    #policy_effect = "Allow" if is_ip_valid and is_auth_valid else "Deny"
+    
     # Construct the Auth Response
     # policy = {
-    #     "principalId": "user",
+    #     "principalId": "uzer",
     #     "policyDocument": {
     #         "Version": "2012-10-17",
     #         "Statement": [
     #             {
     #                 "Action": "execute-api:Invoke",
     #                 "Effect": policy_effect,
-    #                 "Resource": f"arn:aws:execute-api:us-east-1:{ACC_ID}:{API_ID}/{STAGE}/{METHOD}{ROUTE}"  # Use the route ARN from the event
+    #                 "Resource": f"arn:aws:execute-api:us-east-1:{ACC_ID}:{API_ID}/{STAGE}/{METHOD}/*"  # Use the route ARN from the event
     #             }
-    #         ],
-    #     },
-    #     "context": {"exampleKey": "exampleValue"},
+    #         ]
+    #     }
     # }
 
     # return policy
